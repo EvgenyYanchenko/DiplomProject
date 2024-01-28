f824a529835455b578ed9a983fa36a37ea66bc8b
User Geka <evgeny.yanchenko@gmail.com>
2024-01-28 21:35:06 +0300

    add some html files

--- src/main/java/com/example/AIS/controllers/MainController.java
+++ src/main/java/com/example/AIS/controllers/MainController.java
@@ -11,6 +11,7 @@ public class MainController {
     @GetMapping("/")
     public String loginPage(Model model) {
         System.out.println("Helo world new project");
+        System.out.println("привет как дела?7/10");
         return "/login";
     }
 
