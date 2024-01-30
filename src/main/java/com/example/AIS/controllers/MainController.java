de92215876127911f82cab44f8a5d41efefb1011
User Geka <evgeny.yanchenko@gmail.com>
2024-01-30 14:10:15 +0300

    add logo in footer, header, logo & main_Menu page

--- src/main/java/com/example/AIS/controllers/MainController.java
+++ src/main/java/com/example/AIS/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.example.AIS.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -15,5 +16,16 @@ public class MainController {
         return "/login";
     }
 
+//    @PostMapping("/")
+//    public String openMainMenu(Model model)
+//    {
+//        return "redirect:/new_mainMenu";
+//    }
+    @GetMapping("/new_mainMenu")
+    public String mainMenuPage(Model model) {
+        System.out.println("Helo world new project");
+        System.out.println("привет как дела?7/10");
+        return "new_mainMenu";
+    }
 
 }
